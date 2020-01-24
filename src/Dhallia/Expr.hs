86c42a8f777a014254e8fb9faf539db98089eb00
Gregory.Hale <Gregory.Hale@target.com>
2020-01-24 15:07:48 -0500

    Drop inputExprWithM

--- src/Dhallia/Expr.hs
+++ src/Dhallia/Expr.hs
@@ -17,7 +17,6 @@
 
 module Dhallia.Expr (
   Expr
-  , inputExprWithM
   ) where
 
 import qualified Control.Monad.IO.Class     as IO
@@ -37,20 +36,3 @@ import qualified Lens.Family
 -- | Our @Expr@ specializes the one from @Dhall@ with the assumption
 --   that expressions still have source spans and all imports have been resolved
 type Expr = Dhall.Expr Src Void
-
--- |
-inputExprWithM
-  :: IO.MonadIO m => Dhall.NormalizerM m Void
-  -> Dhall.Context Expr
-  -> Text
-  -> m Expr
-inputExprWithM n ctx txt = do
-  let settings = Dhall.defaultInputSettings
-  expr <- Dhall.throws (Dhall.Parser.exprFromText (Lens.Family.view Dhall.sourceName settings) txt)
-  let ctx' = List.foldl' (\acc (k,v) -> Dhall.insert k v acc)
-                         (Lens.Family.view Dhall.startingContext settings)
-                         (Dhall.toList ctx)
-  let status = Lens.Family.set Dhall.Import.startingContext ctx (Dhall.Import.emptyStatus ".")
-  expr' <- IO.liftIO $ State.evalStateT (Dhall.Import.loadWith expr) status
-  _ <- Dhall.throws (Dhall.TypeCheck.typeWith ctx' expr')
-  Dhall.normalizeWithM n expr'
